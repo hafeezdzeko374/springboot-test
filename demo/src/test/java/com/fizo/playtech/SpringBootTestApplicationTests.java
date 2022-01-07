88bf8c889588b0b7619bf87158a0af95c3cabfc5
hafeezdzeko374 <hafeezdzeko374@gmail.com>
2022-01-07 15:15:51 +0800

    initial commit

---
+++ demo/src/test/java/com/fizo/playtech/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizo.playtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
